e65c09dca4ac920dd9249a65df55fcca85cd8afb
sergeicu <serge.vasylechko@gmail.com>
2021-01-22 12:52:01 -0600

    Update runs_python_script_outside_docker.sh

--- batch_process/runs_python_script_outside_docker.sh
+++ batch_process/runs_python_script_outside_docker.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # PURPOSE: 
-# Invoke docker container automatically and process files inside a given directory with src/run_anima.py script. 
-# Important - if you modify the contents of src/run_anima.py - you MUST rebuild the docker image.  
+# Invoke docker container automatically and process files inside a given directory with example_data/run_anima.py script. 
+# If you modify the contents of example_data/run_anima.py - you do NOT need to rebuild the docker image.  
 
 # USAGE: 
-# chmod u+x runs_python_script_inside_docker.sh 
-# ./runs_python_script_inside_docker.sh <path_to_folder>
-
-# IMPORTANT 
-# path_to_folder must have read-write permissions for ALL users: `chmod -R ugo+rwx <path_to_folder>`
+# chmod u+x runs_python_script_outside_docker.sh 
+# chmod -R ugo+rw ../example_data 
+# ./runs_python_script_outside_docker.sh ../example_data/
 
 # ERRORS: 
 # If docker throws error 
 # 1. Check that you can run `sudo` on this machine 
 # 2. Check if you had started docker daemon on this machine: `sudo systemctl start docker`
 
-data=$1
-sudo docker run -it --rm -v $data:/data sergeicu/anima python run_anima.py /data
+data=../example_data/
+sudo docker run -it --rm -v $data:/data sergeicu/anima python /data/run_anima.py /data
 
